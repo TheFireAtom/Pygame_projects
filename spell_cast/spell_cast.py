bc60baedcc4fa49e879bfdee478a8840952366a1
Ivan Kuleshov <64702232+TheFireAtom@users.noreply.github.com>
2024-03-17 11:59:48 +0300

    reorder some code

--- spell_cast/spell_cast.py
+++ spell_cast/spell_cast.py
@@ -4,8 +4,24 @@ from random import randint
 
 # classes
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+
 # functions 
 def game_over_screen():
+    # file imports
+    mage_1_title_surf = pygame.transform.scale2x(mage_1_surf)
+    mage_1_title_rect = mage_1_title_surf.get_rect()
+
+    # rendering text and creating rectangles for it 
+    title_surf_1 = main_font.render("Spellcast 2D", False, BLACK).convert_alpha()
+    title_surf_2 = main_font.render("Press \"ENTER\" to play", False, BLACK).convert_alpha()
+    title_rect_1 = title_surf_1.get_rect()
+    title_rect_2 = title_surf_2.get_rect()
+
+    # drawing screen and positioning images
     screen.fill(PURPLE)
     title_rect_1.center = (400, 50)
     title_rect_2.center = (400, 340)
@@ -15,6 +31,11 @@ def game_over_screen():
     screen.blit(mage_1_title_surf, mage_1_title_rect)
 
 def draw_game():
+    # file imports
+    cave_background_surf = pygame.image.load("images/cave_background/cave_background.png").convert_alpha()
+    stone_ground_surf = pygame.image.load("images/stone_ground/stone_ground.png").convert_alpha()
+
+    # drawing screen and positioning images
     mage_1_rect.center = (400, 350)
     goblin_1_rect.center = (150, 330)
     bat_1_rect.center = (650, 300)
@@ -33,32 +54,24 @@ PURPLE = "#5B115B"
 pygame.init()
 screen_width = 800
 screen_height = 400
-game_active = True # change to False in future development
+game_active = False 
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Spellcast 2D") 
 
-# file imports
+# global file imports
+# player
 main_font = pygame.font.Font("fonts/VisitorRus.ttf", 60)
 mage_1_surf = pygame.image.load("images/mage/mage1.png").convert_alpha()
-mage_1_title_surf = pygame.transform.scale2x(mage_1_surf)
-cave_background_surf = pygame.image.load("images/cave_background/cave_background.png").convert_alpha()
-stone_ground_surf = pygame.image.load("images/stone_ground/stone_ground.png").convert_alpha()
+
+# enemies
 goblin_1_surf = pygame.image.load("images/goblin/goblin.png").convert_alpha()
 bat_1_surf = pygame.image.load("images/bat/bat.png").convert_alpha()
 
-# text render
-title_surf_1 = main_font.render("Spellcast 2D", False, BLACK).convert_alpha()
-title_surf_2 = main_font.render("Press \"ENTER\" to play", False, BLACK).convert_alpha()
-
 #rectangles creation
-title_rect_1 = title_surf_1.get_rect()
-title_rect_2 = title_surf_2.get_rect()
-mage_1_title_rect = mage_1_title_surf.get_rect()
 mage_1_rect = mage_1_surf.get_rect()
 goblin_1_rect = goblin_1_surf.get_rect()
 bat_1_rect = bat_1_surf.get_rect()
-
 
 # running game
 while True:
@@ -67,19 +80,19 @@ while True:
             pygame.quit()
             sys.exit()
 
-        if game_active:
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-        else: 
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_KP_ENTER:
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            pygame.quit()
+            sys.exit() 
+             
+        if game_active == False:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
                 game_active = True
-
-    if game_active == False:
-        game_over_screen()
-
+                
     if game_active:
         draw_game()
+    else:
+        game_over_screen()
+    
 
 
 
